e221e98ad3f84eb8dc969aa0c0700e1f146782c7
Edwin Sumbayak <edwinsumbayak01@gmail.com>
2019-07-25 11:53:03 +0700

    //

---
+++ application/views/admin/admin_footer.php
@@ -0,0 +1,28 @@
+<!-- Jquery JS-->
+<script src="<?= base_url('assets/admin/'); ?>vendor/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap JS-->
+<script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<!-- Vendor JS       -->
+<script src="<?= base_url('assets/admin/'); ?>vendor/slick/slick.min.js">
+</script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/wow/wow.min.js"></script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/animsition/animsition.min.js"></script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+</script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/counter-up/jquery.counterup.min.js">
+</script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/circle-progress/circle-progress.min.js"></script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="<?= base_url('assets/admin/'); ?>vendor/select2/select2.min.js">
+</script>
+
+<!-- Main JS-->
+<script src="<?= base_url('assets/admin/'); ?>js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
