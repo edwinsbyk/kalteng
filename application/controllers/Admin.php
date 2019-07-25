922a2d1f5445a6a872fd0ca249b0ed1fbe2fa2e2
Dedygans <48194882+Dedygans@users.noreply.github.com>
2019-07-25 07:58:42 +0200

    kkk

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,6 +13,66 @@ class Admin extends CI_Controller
 
     public function index()
     {
+    	$this->load->view('admin/header');
         $this->load->view('admin/index');
     }
+
+
+    public function pegawai()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/pegawai');
+    }
+
+
+    public function bidang()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/bidang');
+    }
+
+        public function berita()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/berita');
+    }
+
+        public function artikel()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/artikel');
+    }
+
+        public function agenda()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/agenda');
+    }
+
+        public function testimoni()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/testimoni');
+    }
+
+        public function rup()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/rup');
+    }
+        public function pengumuman_lelang()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/pengumuman_lelang');
+    }
+        public function pemenang_lelang()
+    {
+        $this->load->view('admin/header');
+        $this->load->view('admin/pemenang_lelang');
+    }
+   
+
+
+
+
 }
