57743e7dd9f79cb065e9ab705880c440c5ad87d3
Edwin Sumbayak <edwinsumbayak01@gmail.com>
2019-07-25 17:04:05 +0700

    //

--- application/views/admin/datapu/jalan.php
+++ application/views/admin/datapu/jalan.php
@@ -1,39 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- <!DOCTYPE html> -->
+<!-- <html lang="en"> -->
 
-<head>
-    <!-- Required meta tags-->
-    <meta charset="UTF-8">
+<!-- <head> -->
+<!-- Required meta tags-->
+<!-- <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="au theme template">
     <meta name="author" content="Hau Nguyen">
-    <meta name="keywords" content="au theme template">
+    <meta name="keywords" content="au theme template"> -->
 
-    <!-- Title Page-->
-    <title>Dashboard</title>
+<!-- Title Page-->
+<!-- <title>Dashboard</title> -->
 
-    <!-- Fontfaces CSS-->
-    <link href="<?= base_url('assets/admin/'); ?>css/font-face.css" rel="stylesheet" media="all">
+<!-- Fontfaces CSS-->
+<!-- <link href="<?= base_url('assets/admin/'); ?>css/font-face.css" rel="stylesheet" media="all">
     <link href="<?= base_url('assets/admin/'); ?>vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
     <link href="<?= base_url('assets/admin/'); ?>vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="<?= base_url('assets/admin/'); ?>vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="<?= base_url('assets/admin/'); ?>vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all"> -->
 
-    <!-- Bootstrap CSS-->
-    <link href="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+<!-- Bootstrap CSS-->
+<!-- <link href="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all"> -->
 
-    <!-- Vendor CSS-->
-    <link href="<?= base_url('assets/admin/'); ?>vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+<!-- Vendor CSS-->
+<!-- <link href="<?= base_url('assets/admin/'); ?>vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
     <link href="<?= base_url('assets/admin/'); ?>vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
     <link href="<?= base_url('assets/admin/'); ?>vendor/wow/animate.css" rel="stylesheet" media="all">
     <link href="<?= base_url('assets/admin/'); ?>vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
     <link href="<?= base_url('assets/admin/'); ?>vendor/slick/slick.css" rel="stylesheet" media="all">
     <link href="<?= base_url('assets/admin/'); ?>vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="<?= base_url('assets/admin/'); ?>vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+    <link href="<?= base_url('assets/admin/'); ?>vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all"> -->
 
-    <!-- Main CSS-->
-    <link href="<?= base_url('assets/admin/'); ?>css/theme.css" rel="stylesheet" media="all">
+<!-- Main CSS-->
+<!-- <link href="<?= base_url('assets/admin/'); ?>css/theme.css" rel="stylesheet" media="all"> -->
 
-</head>
+</!-->
 
 
 
@@ -123,10 +123,10 @@
         <div class="container-fluid">
             <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Data PU Jalan</a>
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#table" role="tab" aria-controls="home" aria-selected="true">Data PU Jalan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Tambah Data</a>
+                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#tambah" role="tab" aria-controls="profile" aria-selected="false">Tambah Data</a>
                 </li>
             </ul>
             <div class="tab-content" id="myTabContent">
@@ -316,30 +316,30 @@
         </div>
 
         <!-- Jquery JS-->
-        <script src="<?= base_url('assets/admin/'); ?>vendor/jquery-3.2.1.min.js"></script>
+        <!-- <script src="<?= base_url('assets/admin/'); ?>vendor/jquery-3.2.1.min.js"></script> -->
         <!-- Bootstrap JS-->
-        <script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/popper.min.js"></script>
-        <script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/bootstrap.min.js"></script>
+        <!-- <script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/popper.min.js"></script> -->
+        <!-- <script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-4.1/bootstrap.min.js"></script> -->
         <!-- Vendor JS       -->
-        <script src="<?= base_url('assets/admin/'); ?>vendor/slick/slick.min.js">
-        </script>
-        <script src="<?= base_url('assets/admin/'); ?>vendor/wow/wow.min.js"></script>
+        <!-- <script src="<?= base_url('assets/admin/'); ?>vendor/slick/slick.min.js"> -->
+        <!-- </script> -->
+        <!-- <script src="<?= base_url('assets/admin/'); ?>vendor/wow/wow.min.js"></script>
         <script src="<?= base_url('assets/admin/'); ?>vendor/animsition/animsition.min.js"></script>
-        <script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
-        </script>
-        <script src="<?= base_url('assets/admin/'); ?>vendor/counter-up/jquery.waypoints.min.js"></script>
-        <script src="<?= base_url('assets/admin/'); ?>vendor/counter-up/jquery.counterup.min.js">
-        </script>
-        <script src="<?= base_url('assets/admin/'); ?>vendor/circle-progress/circle-progress.min.js"></script>
+        <script src="<?= base_url('assets/admin/'); ?>vendor/bootstrap-progressbar/bootstrap-progressbar.min.js"> -->
+        <!-- </script> -->
+        <!-- <script src="<?= base_url('assets/admin/'); ?>vendor/counter-up/jquery.waypoints.min.js"></script>
+        <script src="<?= base_url('assets/admin/'); ?>vendor/counter-up/jquery.counterup.min.js"> -->
+        <!-- </script> -->
+        <!-- <script src="<?= base_url('assets/admin/'); ?>vendor/circle-progress/circle-progress.min.js"></script>
         <script src="<?= base_url('assets/admin/'); ?>vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
         <script src="<?= base_url('assets/admin/'); ?>vendor/chartjs/Chart.bundle.min.js"></script>
-        <script src="<?= base_url('assets/admin/'); ?>vendor/select2/select2.min.js">
-        </script>
+        <script src="<?= base_url('assets/admin/'); ?>vendor/select2/select2.min.js"> -->
+        <!-- </script> -->
 
         <!-- Main JS-->
-        <script src="<?= base_url('assets/admin/'); ?>js/main.js"></script>
+        <!-- <script src="<?= base_url('assets/admin/'); ?>js/main.js"></script> -->
 
-        </body>
+        <!-- </body> -->
 
-</html>
-<!-- end document-->
+        </!-->
+        <!-- end document-->
