20ce92f874bca13a05fb8aa329da0ae725008344
Dedygans <48194882+Dedygans@users.noreply.github.com>
2019-07-25 10:36:46 +0200

    kkk

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -63,44 +63,36 @@ class Admin extends CI_Controller
 
         public function testimoni()
     {
-        $this->load->view('admin/header');
-        $this->load->view('admin/testimoni');
+        $this->loadAsset(["admin/testimoni"]);
     }
 
         public function rup()
     {
-              $this->loadAsset(["admin/rup"]);
+              $this->loadAsset(["admin/pengumuman/rup"]);
 
     }
            public function tambahrup()
     {
-         $this->load->view('admin/admin_header');
-        $this->load->view('admin/header');
-        $this->load->view('admin/tambahrup');
-        $this->load->view('admin/admin_footer');
+        $this->loadAsset(["admin/pengumuman/tambahrup"]);
     }
 
         public function pengumuman_lelang()
     {
-        $this->load->view('admin/header');
-        $this->load->view('admin/pengumuman_lelang');
+         $this->loadAsset(["admin/pengumuman/pengumuman_lelang"]);
     }
     public function tambahpengumuman()
     {
-        $this->load->view('admin/header');
-        $this->load->view('admin/tambahpengumuman');
+        $this->loadAsset(["admin/pengumuman/tambahpengumuman"]);
     }
 
         public function pemenang_lelang()
     {
-        $this->load->view('admin/header');
-        $this->load->view('admin/pemenang_lelang');
+         $this->loadAsset(["admin/pengumuman/pemenang_lelang"]);
     }
 
        public function tambahpemenanglelang()
     {
-        $this->load->view('admin/header');
-        $this->load->view('admin/tambahpemenanglelang');
+        $this->loadAsset(["admin/pengumuman/tambahpemenanglelang   "]);
     }
 
     
