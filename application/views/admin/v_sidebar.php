d5e1ff230e5ec82cdd78391a29e3b56ec162690d
syahidnurrohim <syahidnurrohim@gmail.com>
2019-07-29 11:35:13 +0700

    sidebar fix

--- application/views/admin/v_sidebar.php
+++ application/views/admin/v_sidebar.php
@@ -7,11 +7,14 @@
     <div class="menu-sidebar__content js-scrollbar1">
         <nav class="navbar-sidebar">
             <ul class="list-unstyled navbar__list">
-                <?php foreach ($menu as $m): ?>
+                <?php 
+                    $i = 0;
+                    foreach ($menu as $m): 
+                ?>
                     <li class="<?php
                         echo count($m->sub_menu) ? "has-sub " : ""; 
                         echo $this->uri->uri_string() == $m->module_path || in_array($this->uri->uri_string(), array_map(function($v){return $v->module_sub_path;}, $m->sub_menu)) ? "active" : ""?> list-sidebar-item">
-                        <a <?= count($m->sub_menu) ? "class='js-arrow' href='#'" : "href='".base_url($m->module_path)."'" ?> >
+                        <a <?= count($m->sub_menu) ? "class='js-arrow' href='#' data-index='$i' " : "href='".base_url($m->module_path)."'" ?> >
                             <i class="<?= $m->module_icon ?>"></i><?= $m->module_name ?>
                         </a>
                             <?php if (count($m->sub_menu)): ?>
@@ -24,7 +27,7 @@
                                 </ul>
                             <?php endif; ?>
                     </li>
-                <?php endforeach; ?>
+                <?php count($m->sub_menu) && $i++; endforeach; ?>
             </ul>
         </nav>
     </div>
