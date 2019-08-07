336b372eb67d0c8dc0564303db5909222efc20d6
Edwin Sumbayak <edwinsumbayak01@gmail.com>
2019-08-07 11:33:09 +0700

    download folder

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,82 +1,67 @@
 <!-- Footer -->
 <div class="footer-v1 bg-dark">
-        <div class="footer bg-dark">
-            <div class="container">
-                <div class="row">
-                    <!-- About -->
-                    <div class="col-md-6 md-margin-bottom-40">
-                        <div class="headline"><h2 class="text-light bg-dark">Tentang Kalteng</h2></div>
-                        <img src="//4.bp.blogspot.com/-a0rStfWG9nM/UNmcMwUsGwI/AAAAAAAAIwc/AcCmv02dmiI/s1600/LOGO+PROVINSI+KALIMANTAN+TENGAH.png" alt="Logo" style="height: 75px; float: left; margin-right: 5px; margin-bottom: 5px;">
-                        <p class="text-light bg-dark"><b class="text-light bg-dark">Kalimantan Tengah</b> adalah salah satu provinsi di Indonesia yang terletak di Pulau Kalimantan. Ibukotanya adalah Kota Palangka Raya. Kalimantan Tengah memiliki luas 157.983 km².</p>
-                    </div><!--/col-md-3-->
-                    <!-- End About -->
-
-                    <!-- Latest -->
-                    <div class="col-md-6 md-margin-bottom-40">
-                        <div class="posts">
-                            <div class="headline " ><h2 class="text-light bg-dark">Kontak Info</h2></div>
-                            <ul class="list-unstyled latest-list">
-                                <li>
-                                    <a href="#" class="text-light bg-dark">Alamat</a>
-                                    <small class="text-light bg-dark">Jl. S. Parman No.3, Palangka, Kec. Jekan Raya, Kota Palangka Raya, Kalimantan Tengah 73111
-                                | Dinas Pekerjaan Umum dan Penataan Ruang Provinsi Kalteng |</small>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-light bg-dark">Telp.</a>
-                                    <small class="text-light bg-dark">(0536) 3235336</small>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-light bg-dark"> Email</a>
-                                    <small class="text-light bg-dark">diskominfo@kalteng.go.id</small>
-                                </li>
-                            </ul>
-                        </div>
-                    </div><!--/col-md-3-->
-                    <!-- End Latest -->
-
-                    <!-- Link List -->
-                   
-        </div><!--/footer-->
-
-        <div class="copyright">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <p class="p-3 mb-2 bg-dark text-white">
-                            2019 © PEMERINTAH PROVINSI KALIMANTAN TENGAH 
-                        </p>
+    <div class="footer bg-dark">
+        <div class="container">
+            <div class="row">
+                <!-- About -->
+                <div class="col-md-6 md-margin-bottom-40">
+                    <div class="headline">
+                        <h2 class="text-light bg-dark">Tentang Kalteng</h2>
                     </div>
+                    <img src="//4.bp.blogspot.com/-a0rStfWG9nM/UNmcMwUsGwI/AAAAAAAAIwc/AcCmv02dmiI/s1600/LOGO+PROVINSI+KALIMANTAN+TENGAH.png" alt="Logo" style="height: 75px; float: left; margin-right: 5px; margin-bottom: 5px;">
+                    <p class="text-light bg-dark"><b class="text-light bg-dark">Kalimantan Tengah</b> adalah salah satu provinsi di Indonesia yang terletak di Pulau Kalimantan. Ibukotanya adalah Kota Palangka Raya. Kalimantan Tengah memiliki luas 157.983 km².</p>
+                </div>
+                <!--/col-md-3-->
+                <!-- End About -->
 
-                   
+                <!-- Latest -->
+                <div class="col-md-6 md-margin-bottom-40">
+                    <div class="posts">
+                        <div class="headline ">
+                            <h2 class="text-light bg-dark">Kontak Info</h2>
+                        </div>
+                        <ul class="list-unstyled latest-list">
+                            <li>
+                                <a href="#" class="text-light bg-dark">Alamat</a>
+                                <small class="text-light bg-dark">Jl. S. Parman No.3, Palangka, Kec. Jekan Raya, Kota Palangka Raya, Kalimantan Tengah 73111
+                                    | Dinas Pekerjaan Umum dan Penataan Ruang Provinsi Kalteng |</small>
+                            </li>
+                            <li>
+                                <a href="#" class="text-light bg-dark">Telp.</a>
+                                <small class="text-light bg-dark">(0536) 3235336</small>
+                            </li>
+                            <li>
+                                <a href="#" class="text-light bg-dark"> Email</a>
+                                <small class="text-light bg-dark">diskominfo@kalteng.go.id</small>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--/col-md-3-->
+                <!-- End Latest -->
+
+                <!-- Link List -->
+
+            </div>
+            <!--/footer-->
+
+            <div class="copyright">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p class="p-3 mb-2 bg-dark text-white">
+                                2019 © PEMERINTAH PROVINSI KALIMANTAN TENGAH
+                            </p>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
-        </div><!--/copyright-->
-    </div>
+            <!--/copyright-->
+        </div>
 
 
-<?php foreach ($js_list as $js) { ?>
-    <script src="<?= base_url($js) ?>"></script>
-<?php } ?>
-<script>
-    $('.custom-file-input').on('change', function() {
-        let fileName = $(this).val().split('\\').pop();
-        $(this).next('.custom-file-label').addClass("selected").html(fileName);
-    });
-
-    $('.form-check-input').on('click', function() {
-        const menuId = $(this).data('menu');
-        const roleId = $(this).data('role');
-
-        $.ajax({
-            url: "<?= base_url('admin/changeaccess'); ?>",
-            type: 'post',
-            data: {
-                menuId: menuId,
-                roleId: roleId
-            },
-            success: function() {
-                document.location.href = "<?= base_url('admin/roleaccess/') ?>" + roleId;
-            }
-        });
-    });
-</script>
+        <?php foreach ($js_list as $js) { ?>
+            <script src="<?= base_url($js) ?>"></script>
+        <?php } ?>
